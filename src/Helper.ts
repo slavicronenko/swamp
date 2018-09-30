de17167e74191ba7bb2143f39b5ce1d44716d6b6
viacheslav.ronenko <slavic.ronenko@gmail.com>
2018-09-30 23:26:42 +0300

    Feature: Add few function to the helper

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,4 +1,15 @@
-export const requestAnimationFrame = (<any>window).requestAnimationFrame
-                                  || (<any>window).webkitRequestAnimationFrame
-                                  || (<any>window).mozRequestAnimationFrame
-                                  || (<any>window).msRequestAnimationFrame;
+export function random(
+  givenMin: number = 0,
+  givenMax: number = Number.MAX_SAFE_INTEGER,
+  precision: number = 0
+): number {
+  const multiplier = Math.pow(10, precision);
+  const min = givenMin * multiplier;
+  const max = givenMax * multiplier;
+
+  return (Math.floor(Math.random() * (max - min)) + min) / multiplier;
+}
+
+export function isFunction(entity: any) {
+  return typeof entity === 'function';
+}
