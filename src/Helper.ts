70d5d6ba5f299e439c42c2660858cc33b5cf02a2
Viacheslav Ronenko <slavic.ronenko@gmail.com>
2018-10-03 18:48:48 +0300

    Feature: Improve string generation method

--- src/Helper.ts
+++ src/Helper.ts
@@ -2,11 +2,11 @@ export function random(min: number = 0, max: number = Number.MAX_SAFE_INTEGER, p
   return +(Math.random() * (max - min) + min).toFixed(precision);
 }
 
-export function generateString(size: number = 6, chars: string = 'abcdef0123456789'): string {
+export function generateString(size: number = 6, chars: string = 'abcdefghijklmnopqrstuvwxyz'): string {
   return Array(size)
     .fill(null)
     .map(() => chars[random(0, chars.length - 1)])
-    .join();
+    .join('');
 }
 
 export function isFunction(entity: any) {
